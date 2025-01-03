9c824d1165f5a93d58ffce8a4931db09b3477692
MarvinDinaku <marvindinaku@gmal.com>
2025-01-03 15:32:46 +0100

    asd

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -90,7 +90,7 @@
 
             <!-- Summary Field -->
             <div class="mb-3">
-                <label for="summary" class="form-label">Modified Content</label>
+                <label for="summary" class="form-label">Modified Post Content</label>
                 <textarea name="summary" id="summary" class="form-control @error('summary') is-invalid @enderror" rows="3">{{ old('summary', $post->summary) }}</textarea>
                 @error('summary')
                 <div class="invalid-feedback">
